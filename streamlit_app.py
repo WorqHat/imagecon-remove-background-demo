ee44ee84d3ec01a1ea9eb758974c1ffd0db135d1
Sagnik Ghosh <sagnik@worqhat.com>
2024-04-02 03:13:15 +0530

    Create the example for Remove BG

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,71 @@
-import streamlit as st
 import requests
 import time
-import random
-# Function to call the API for v2
-def call_apiv2(prompt):
+import streamlit as st
+
+
+def callapi(file, api_key):
     start_time = time.time()  # Record start time
-    url = "https://api.worqhat.com/api/ai/images/generate/v2"
-    payload = {
-        "orientation": "square",
-        "output_type": "url",
-        "prompt": [prompt]
-    }
+    url = "https://api.worqhat.com/api/ai/images/modify/v3/remove-background"
+    payload = {"output_type": "url"}
+    files = {'existing_image': file}
     headers = {
-        "Authorization": "Bearer sk-02e44d2ccb164c738a6c4a65dbf75e89",
-        "Content-Type": "application/json"
+        'Accept': 'application/json',
+        "Authorization": f"Bearer {api_key}"  # Use the api_key variable
     }
-    response = requests.post(url, json=payload, headers=headers)
+    st.write("The request is being sent")
+    response = requests.request(
+        "POST", url, headers=headers, data=payload, files=files)
     end_time = time.time()  # Record end time
     duration = end_time - start_time  # Calculate duration
     if response.status_code == 200:
         return response.json()['image'], round(duration, 2)
     else:
+        st.write("The request failed")
         return None, round(duration, 2)
 
-# Function to call the API for v3
-def call_apiv3(prompt):
-    start_time = time.time()  # Record start time
-    url = "https://api.worqhat.com/api/ai/images/generate/v3"
-    payload = {
-        "orientation": "square",
-        "output_type": "url",
-        "prompt": [prompt]
-    }
-    headers = {
-        "Authorization": "Bearer sk-02e44d2ccb164c738a6c4a65dbf75e89",
-        "Content-Type": "application/json"
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    end_time = time.time()  # Record end time
-    duration = end_time - start_time  # Calculate duration
-    if response.status_code == 200:
-        return response.json()['image'], round(duration, 2)
+
+st.title("Remove Image Background using ImageCon API")
+st.write("This API enables the removal of the background from images in a single API call, delivering a clear and focused foreground subject without the original backdrop.")
+
+st.markdown(
+    "To use this API, you need an API key. If you don't have one, please sign up at [WorqHat API Signup Page](https://app.worqhat.com).")
+
+uploaded_image = st.file_uploader(
+    "Choose an image...", type=["jpg", "jpeg", "png"])
+
+# Check if an image has been uploaded
+if uploaded_image is not None:
+    # Check the size of the uploaded file
+    file_size = uploaded_image.size
+    # Convert bytes to megabytes
+    file_size_mb = file_size / (1024 * 1024)
+
+    # If the file size exceeds 16 MB, display a warning and set uploaded_image to None
+    if file_size_mb > 16:
+        st.warning(
+            "The uploaded file exceeds the 16 MB size limit. Please upload a smaller file.")
+        uploaded_image = None
     else:
-        return None, round(duration, 2)
+        # Proceed with processing the uploaded image
+        st.success("File uploaded successfully.")
 
-st.title("AI Image Generator")
-st.write("Test these features and more on [Worqhat](https://worqhat.com).")
-st.write("Checkout our [Documentation](https://docs.worqhat.com/introduction)")
+api_key = st.text_input("Enter WorqHat API Key:", type="password")
 
-prompt = st.text_input("Enter prompt:")
-col1, col2 = st.columns([1,1])
-with col1:
-    if st.button("Send"):
-        st.write("Generating Image...")
-        response_v2, duration_v2 = call_apiv2(prompt)
-        with col1:
-            st.image(response_v2, caption=f"Generated Image (v2) - Time taken: {duration_v2} seconds", use_column_width=True)
-        response_v3, duration_v3 = call_apiv3(prompt)
-        with col2:
-            st.image(response_v3, caption=f"Generated Image (v3) - Time taken: {duration_v3} seconds", use_column_width=True)
-        
-with col2:
-    if st.button("Surprise Me !"):
-        st.write("Generating Image...")
-        prompts=["ethereal glowing holographic crystalised moth on a frosty spring morning","high quality, 8K Ultra HD, A beautiful double exposure that combines an goddess silhouette with sunset coast, sunset coast should serve as the underlying backdrop, with its details incorporated into the goddess , crisp lines, The background is monochrome, sharp focus, double exposure, awesome full color","A cat bunny hybrid","whimiscally cute and adorable little baby foxy cuteness overload, wonderfully dreamy atmosphere filled with a whimsical mix of pastel colors, with, golden shaded background, huge flower explosion"]
-        prompt = random.choice(prompts)  
-        response_v2, duration_v2 = call_apiv2(prompt)
-        with col1:
-            st.image(response_v2, caption=f"Generated Image (v2) - Time taken: {duration_v2} seconds", use_column_width=True)
-        response_v3, duration_v3 = call_apiv3(prompt)
-        with col2:
-            st.image(response_v3, caption=f"Generated Image (v3) - Time taken: {duration_v3} seconds", use_column_width=True)
+if st.button("Send") and uploaded_image is not None:
+    st.write("Generating the image ")
+    response_v2, duration_v2 = callapi(uploaded_image, api_key)
+
+    col1, col2 = st.columns(2)  # Create two columns
+    with col1:
+        st.image(uploaded_image, caption="Input image")
+    with col2:
+        if response_v2:
+            st.image(
+                response_v2, caption=f"Modified image - Time taken: {duration_v2} seconds")
+        else:
+            st.write("Failed to generate the image.")
+
+st.write("This API enables the removal of the background from images in a single API call, delivering a clear and focused foreground subject without the original backdrop.")
+
+st.markdown(
+    "For more information, visit the [Documentation page](https://docs.worqhat.com/ai-models/image-generation/remove-image-background) and the [API Reference page](https://docs.worqhat.com/api-reference/ai-models/image-generation/remove-image-background).")
